9a71b0c681e02e85eda91a1b98495af9f0774304
spell-landon <spell.landon@gmail.com>
2022-01-10 21:06:10 -0600

    Change Hero message

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,9 +15,15 @@ function Home(props) {
     <div className={styles.homeContainer}>
       <h1>Welcome to Job Search Tracker</h1>
       <p>
-        Here you can add job interviews to a tracker so you can see all the jobs
-        that you have applied to and update accordingly!
+        Track the endless job search, store info about upcoming
+        interviews, and keep your data secure, all in a fresh new design.
       </p>
+
+      <section>
+      <p>There's a light at the end of the tunnel.</p>
+        <p>Track your job search.</p>
+        <p>Track your future.</p>
+      </section>
 
       {user.username ? (
         <Link to='/dashboard'>
