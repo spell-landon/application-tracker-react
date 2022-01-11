8c6b562d4aab60215e992e09f01149c21954266b
spell-landon <spell.landon@gmail.com>
2022-01-10 21:22:18 -0600

    Change loading animations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,14 +15,14 @@ function Home(props) {
     <div className={styles.homeContainer}>
       <h1>Welcome to Job Search Tracker</h1>
       <p>
-        Track the endless job search, store info about upcoming
-        interviews, and keep your data secure, all in a fresh new design.
+        Track the endless job search, store info about upcoming interviews, and
+        keep your data secure, all in a fresh new design.
       </p>
 
       <section>
-      <p>There's a light at the end of the tunnel.</p>
-        <p>Track your job search.</p>
-        <p>Track your future.</p>
+        <p id={styles.first}>There's a light at the end of the tunnel.</p>
+        <p id={styles.second}>Track your job search.</p>
+        <p id={styles.third}>Track your future.</p>
       </section>
 
       {user.username ? (
